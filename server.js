a572945d5f31d0e41be568e61023b1a4411703c6
Camilo Cota <camilocot@gmail.com>
2014-05-06 21:29:31 +0200

    Indent

--- server.js
+++ server.js
@@ -18,15 +18,15 @@ var db = mongoose.connect(config.mongo.uri, config.mongo.options);
 
 // Bootstrap models
 var modelsPath = path.join(__dirname, 'lib/models');
-fs.readdirSync(modelsPath).forEach(function (file) {
-  if (/(.*)\.(js$|coffee$)/.test(file)) {
-    require(modelsPath + '/' + file);
-  }
+fs.readdirSync(modelsPath).forEach(function(file) {
+    if (/(.*)\.(js$|coffee$)/.test(file)) {
+        require(modelsPath + '/' + file);
+    }
 });
 
-if(config.dummydata && process.env.NODE_ENV !== 'test') {
-  // Populate empty DB with sample data
-  require('./lib/config/dummydata');
+if (config.dummydata && process.env.NODE_ENV !== 'test') {
+    // Populate empty DB with sample data
+    require('./lib/config/dummydata');
 }
 
 // Passport Configuration
@@ -38,8 +38,8 @@ require('./lib/config/express')(app);
 require('./lib/routes')(app);
 
 // Start server
-app.listen(config.port, config.ip, function () {
-  console.log('Express server listening on %s:%d, in %s mode', config.ip, config.port, app.get('env'));
+app.listen(config.port, config.ip, function() {
+    console.log('Express server listening on %s:%d, in %s mode', config.ip, config.port, app.get('env'));
 });
 
 // Expose app
