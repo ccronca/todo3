a280c12ad0edf9caee068297c19dad982a193ae8
Camilo Cota <camilocot@gmail.com>
2014-05-17 19:13:48 +0200

    Indent js

--- test/client/spec/directives/todoescape.js
+++ test/client/spec/directives/todoescape.js
@@ -2,19 +2,19 @@
 
 describe('Directive: todoEscape', function() {
 
-    // load the directive's module
-    beforeEach(module('todoApp'));
+  // load the directive's module
+  beforeEach(module('todoApp'));
 
-    var element,
-        scope;
+  var element,
+    scope;
 
-    beforeEach(inject(function($rootScope) {
-        scope = $rootScope.$new();
-    }));
+  beforeEach(inject(function($rootScope) {
+    scope = $rootScope.$new();
+  }));
 
-    // it('should make hidden element visible', inject(function ($compile) {
-    //   element = angular.element('<todo-escape></todo-escape>');
-    //   element = $compile(element)(scope);
-    //   expect(element.text()).toBe('this is the todoEscape directive');
-    // }));
+  // it('should make hidden element visible', inject(function ($compile) {
+  //   element = angular.element('<todo-escape></todo-escape>');
+  //   element = $compile(element)(scope);
+  //   expect(element.text()).toBe('this is the todoEscape directive');
+  // }));
 });
